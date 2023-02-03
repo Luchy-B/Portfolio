1739156e5679b51da4e89d3f8b8dc1bd4a32e928
Luchy-B <okparabon@gmail.com>
2023-02-03 16:57:36 +0100

    Update Linters error

--- JS/scripts.js
+++ JS/scripts.js
@@ -12,3 +12,20 @@ function hide() {
 
 hamburger.addEventListener('click', show);
 closeBtn.addEventListener('click', hide);
+
+// Form validation
+const userEmail = document.querySelector('#email');
+const alertMsg = document.querySelector('.alert-Msg');
+const contactForm = document.querySelector('.input-Form');
+
+contactForm.addEventListener('submit', (event) => {
+  const regex = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  const emailContent = userEmail.value;
+  if (regex.test(emailContent)) {
+    alertMsg.innerHTML = 'Success';
+    alertMsg.style.color = 'green';
+  } else {
+    alertMsg.innerHTML = 'Email address should all be lower case';
+    event.preventDefault();
+  }
+});
