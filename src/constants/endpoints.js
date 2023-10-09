6365af9f5f3890730c16795d94af483cce076e97
Lucas David Erkana <lderkana21x@gmail.com>
2023-10-09 13:22:55 +0200

    All features updated

---
+++ src/constants/endpoints.js
@@ -0,0 +1,11 @@
+const endpoints = {
+  navbar: 'profile/navbar.json',
+  routes: 'profile/routes.json',
+  home: 'profile/home.json',
+  social: 'profile/social.json',
+  about: 'profile/about.json',
+  skills: 'profile/skills.json',
+  projects: 'profile/projects.json',
+};
+
+export default endpoints;
